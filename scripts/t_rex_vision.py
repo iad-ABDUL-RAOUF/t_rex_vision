3192742b5e549af751b8496c5eef26a71b2de8f7
iad-ABDUL-RAOUF <iad_abdulraouf@yahoo.fr>
2021-03-23 22:15:48 +0100

    t_rex_vision develloped. not tested...

--- scripts/t_rex_vision.py
+++ scripts/t_rex_vision.py
@@ -12,7 +12,7 @@ from sensor_msgs.msg import Image
 from sensor_msgs.msg import CompressedImage
 import numpy as np
 import imutils
-import homography
+import homography as hm
 
 def somefunct(arg):
     out = arg
@@ -33,12 +33,14 @@ class MovingObjectDetector:
         # cv_bridge
         self.bridge = cv_bridge.CvBridge()
         # class member
+        self.isImage_ = False
         self.isNewImage_ = False
-        self.imageNow_ = np.array([])
-        self.imagePrev_ = np.array([])
-        self.imageNowCopy_ = imageNow_.copy()
-        self.imagePrevCopy_ = imagePrev_.copy()
-        self.mutex = Lock()
+        self.isProcessInit_ = False
+        self.imageNow_ = None
+        self.imagePrev_ = None
+        self.imageNowProcess_ = None
+        self.imagePrevProcess_ = None
+        self.mutex_ = Lock()
         
 
     def read_parameters(self):
@@ -55,24 +57,111 @@ class MovingObjectDetector:
         rospy.loginfo('Parameter %s has value %s', rospy.resolve_name('~ransac/max_iterations'), self.param['ransac']['max_iterations'])
         self.param['ransac']['stop_at_goal'] = rospy.get_param('~ransac/stop_at_goal', True)
         rospy.loginfo('Parameter %s has value %s', rospy.resolve_name('~ransac/stop_at_goal'), self.param['ransac']['stop_at_goal'])
-        self.param['ransac']['random_seed'] = rospy.get_param('~ransac/random_seed', None)
-        rospy.loginfo('Parameter %s has value %s', rospy.resolve_name('~ransac/random_seed'), self.param['ransac']['random_seed'])
         # other params
-        self.param['downsamplingStep'] = rospy.get_param('~downsamplingStep', 20)
-        rospy.loginfo('Parameter %s has value %s', rospy.resolve_name('~downsamplingStep'), self.param['downsamplingStep'])
+        self.param['step'] = rospy.get_param('~downsamplingStep', 20)
+        rospy.loginfo('Parameter %s has value %s', rospy.resolve_name('~downsamplingStep'), self.param['step'])
 
     def image_callback(self,msg):
-        # conversion ros image vers opencv
-        image = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
-
-        cv2.imshow("Lecture image du drone", image)
+        with mutex_:
+            try:
+                self.imagePrev_ = self.imageNow_.copy()
+            except AttributeError:
+                rospy.logwarn("imageNow_ has no attribute 'copy'. Probably still at init value 'None'")
+            # conversion ros image vers opencv
+            self.imageNow_ = self.bridge.imgmsg_to_cv2(msg,desired_encoding='bgr8')
+            self.isNewImage_ = True
+        if (self.imagePrev_ != None and self.imageNow_ != None)
+            self.isImage_ = True
+        cv2.imshow("debug self.imageNow_", self.imageNow_)
+        cv2.imshow("debug self.imagePrev_", self.imagePrev_)
         cv2.waitKey(1)
-        # conversion et publication image opencv vers message ros
-        self.moving_pub.publish(self.bridge.cv2_to_imgmsg(image, encoding="passthrough"))
         
     def processing(self):
-        # nothing
-        return 0
+        if (self.isNewImage_ and self.isImage_):
+            # load images
+            with mutex_:
+                self.imageNowProcess_ = cv2.cvtColor(self.imageNow_,cv2.COLOR_BGR2GRAY)
+                self.imagePrevProcess_ = cv2.cvtColor(self.imagePrev_,cv2.COLOR_BGR2GRAY)
+                self.isNewImage_ = False
+            
+            # init some processing variable
+            if not(self.isProcessInit_):
+                self.initBeforeFirstProcess()
+            
+            # do processing
+            # compute optical flow. TODO put optical flow parameters in the parameter server
+            self.flow = cv2.calcOpticalFlowFarneback(   self.imagePrevProcess_,self.imageNowProcess_,None, 
+                                                        pyr_scale = 0.5,# Taux de réduction pyramidal
+                                                        levels = 3, # Nombre de niveaux de la pyramide
+                                                        winsize = 15, # Taille de fenêtre de lissage (moyenne) des coefficients polynomiaux
+                                                        iterations = 3, # Nb d'itérations par niveau
+                                                        poly_n = 7, # Taille voisinage pour approximation polynomiale
+                                                        poly_sigma = 1.5, # E-T Gaussienne pour calcul dérivées 
+                                                        flags = 0)
+            # compute down sampled pixel coordinates after they are moved by the optical flow
+            self.ds_h_movedPixels = self.ds_h_pixels + np.concatenate(( self.flow[self.step//2::self.step,self.step//2::self.step,0].reshape((-1,1)),
+                                                                        self.flow[self.step//2::self.step,self.step//2::self.step,1].reshape((-1,1)),
+                                                                        np.zeros((self.ds_h_pixels.size[0],1))),
+                                                                        axis = 1)
+            # compute homography linking pixel before and after they moved. Correspond to global picture mouvement i.e. ego-motion 
+            homography, ds_nIinliers = hm.run_ransac(  self.ds_h_pixels,
+                                                    self.ds_h_movedPixels,
+                                                    self.param['ransac']['threshold'],
+                                                    self.param['ransac']['sample_size'],
+                                                    self.goal_inliers,
+                                                    self.param['ransac']['max_iterations'],
+                                                    self.param['ransac']['stop_at_goal'])[0:2]
+            if ds_nIinliers == None:
+                rospy.logwarn('ego motion model estimation failed')
+            else:
+                # compute outliers in the full image
+                self.h_movedPixels = self.h_pixels + self.flow
+                self.moving_ = np.logical_not(hm.findHomographyInlier(  homography,
+                                                                            self.h_pixels,
+                                                                            self.h_movedPixels,
+                                                                            self.param['ransac']['threshold']).reshape((self.h, self.v)))
+                rospy.loginfo('%s inliers, %s outliers (moving pixel)', self.moving_.sum(), self.h*self.w - self.moving_.sum())
+
+                # conversion and publication of the moving pixels
+                self.moving_pub.publish(self.bridge.cv2_to_imgmsg(self.moving_.astype(np.float64), encoding="mono8"))
+
+                # graphical display
+                self.mag, self.ang = cv2.cartToPolar(flow[:,:,0], flow[:,:,1])
+                self.hsv[:,:,0] = (self.ang*180)/(2*np.pi)
+                self.hsv[:,:,2] = (self.mag*255)/np.amax(self.mag)
+                self.bgr = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
+                cv2.imshow("debug self.imageNowProcess_", self.imageNowProcess_)
+                cv2.imshow("debug self.imagePrevProcess_", self.imagePrevProcess_)
+                cv2.imshow('mooving object',self.moving_.astype(np.float64))
+                cv2.imshow('optical flow',bgr)
+                cv2.waitKey(1)
+                
+                
+    
+    def initBeforeFirstProcess(self):
+        (self.h,self.w,self.c) = self.imageNowProcess_.shape
+        self.step = self.param['step']
+        # homogeneous pixel list 
+        hMat, wMat = np.meshgrid(np.arange(self.h), np.arange(self.w), indexing = 'ij')
+        self.h_pixels = np.concatenate((hMat.reshape((-1,1)),
+                                        wMat.reshape((-1,1)),
+                                        np.ones((self.h*self.w,1))),
+                                        axis = 1)
+        # down sampled homogeneous pixel list 
+        ds_hRange = np.arange(self.step//2,self.h,self.step)
+        ds_wRange = np.arange(self.step//2,self.w,self.step)
+        hMat, wMat = np.meshgrid(ds_hRange, ds_wRange, indexing = 'ij')
+        self.ds_h_pixels = np.concatenate(( hMat.reshape((-1,1)),
+                                            wMat.reshape((-1,1)),
+                                            np.ones((ds_hRange.size*ds_wRange.size,1))),
+                                        axis = 1)
+        # ransac param
+        self.goal_inliers = int(ds_hRange.size*ds_wRange.size*self.param['ransac']['goal_inliers'])
+        # graphical display
+        self.hsv = np.zeros_like(self.imageNowProcess_)
+        self.hsv[:,:,1] = 255 # color saturation
+
+           
         
 
 if __name__ == '__main__':
